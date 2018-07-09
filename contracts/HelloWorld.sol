eacce23db761a6c108816979040f2c00a10fa7c6
ClaudiuSchuster <info@x0e.de>
2018-07-09 00:30:37 +0000

    contracts/*: updated HelloWorld contract

--- contracts/HelloWorld.sol
+++ contracts/HelloWorld.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.4.23;
 
 contract HelloWorld {
  
-    uint256 counter = 4;            //state variable we assigned earlier
+    uint256 counter = 4;            //state variable
+    address owner = msg.sender;     //set owner as msg.sender
     
     function add() public {         //increases counter by 1
         counter++;
@@ -14,4 +15,12 @@ contract HelloWorld {
     function getCounter() public constant returns (uint256) {
         return counter;
     } 
+    function kill() public {        //self-destruct function
+        if(msg.sender == owner) {
+            selfdestruct(owner); 
+        }
+    }
+    function () public payable {
+        counter = 4;                //reset counter
+    }
 }
