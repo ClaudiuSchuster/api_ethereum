817b8fcf1df4115cfeadf24172eb190018a6735a
ClaudiuSchuster <info@x0e.de>
2018-07-10 02:11:25 +0000

    Modulized method subclasses

--- methods/eth.pm
+++ methods/eth.pm
@@ -6,82 +6,54 @@ use strict; use warnings; use utf8; use feature ':5.10';
 use modules::Ethereum;
 ## Load some more from our modules...
 use methods::eth::personal::account;
+use methods::eth::contract;
 
-use Data::Dumper; ### DELETE after DEV
 
 sub run {
     my $cgi = shift;
     my $json = shift;
-    ####################  Initialize some stuff...  #########################
-    $json->{meta}{method} = $json->{meta}{postdata}{method} if($json->{meta}{postdata}{method} eq 'eth');
-    my $params = $json->{meta}{postdata}{params} || undef;
     my $eth = {};
-    #################  Initialize our Ethereum Node  ########################
-    my $node = API::modules::Ethereum->new('http://127.0.0.1:854'.($API::dev?6:5).'/');
-    $node->set_debug_mode(1);
-    $node->set_show_progress(1);
     
-    #########################################################################  eth
-    if( $json->{meta}{postdata}{method} eq "eth" ) {
-        $json->{meta}{method} = $json->{meta}{postdata}{method};
-        
-        my $mathBigIntOBject = $node->eth_getBalance( API::methods::eth::personal::account::address, 'latest' );
-        # $json->{meta}{msg} = Dumper(  );
-        # $json->{meta}{msg} = $node->wei2ether( $mathBigIntOBject )->bstr(); # ->numify()
-        $json->{meta}{msg} = $mathBigIntOBject->bstr();
-        # $json->{meta}{msg} = $node->web3_clientVersion();
-    }    
-    #########################################################################  eth.contract.deploy
-    elsif( $json->{meta}{postdata}{method} eq "eth.contract.deploy" ) {
-        $json->{meta}{method} = $json->{meta}{postdata}{method};
-        if ( ref($params) eq 'HASH' ) {
-            unless( $params->{name} ) {
-                $json->{meta}{rc}  = 400;
-                $json->{meta}{msg} = "Insufficient arguments submitted: 'name' of contract to deploy is needed!";
-            } else {
-                if( -e 'contracts/'.$params->{name}.'.sol' ) {
-                    my $contract_status;
-                    eval {
-                        $contract_status = $node->compile_and_deploy_contract(
-                            $params->{name},
-                            {   # Constructor Params
-                                initString => '+ IceMine.io - The One And Only +',
-                                initValue  => 102,
-                            },
-                            API::methods::eth::personal::account::address,
-                            API::methods::eth::personal::account::password
-                        ); 1; 
-                    } or do { 
-                        $json->{meta}{rc}  = 500;
-                        $json->{meta}{msg} = 'error.eth.contract.deploy: '.$@;
-                    };
-                    if( $json->{meta}{rc} == 200 ) {
-                        my $address     = $contract_status->{contractAddress};
-                        my $txhash      = $contract_status->{transactionHash};
-                        my $gas_used    = hex($contract_status->{gasUsed});
-                        my $gas_price   = $node->eth_gasPrice();
-                        my $tx_cost_wei = $gas_used * $gas_price;
-                        my $tx_cost_eth = $node->wei2ether($tx_cost_wei);
-                        $eth->{contract}{deploy}{address}       = $address;
-                        $eth->{contract}{deploy}{tx}            = $txhash;
-                        $eth->{contract}{deploy}{tx_cost_wei}   = $tx_cost_wei->numify();
-                        $eth->{contract}{deploy}{tx_cost_eth}   = $tx_cost_eth->numify();
-                        $eth->{contract}{deploy}{gas_price_wei} = $gas_price->numify();
-                        $eth->{contract}{deploy}{gas_used}      = $gas_used;
-                    }
-                } else {
-                    $json->{meta}{rc}  = 400;
-                    $json->{meta}{msg} = "Contract 'contracts/$params->{name}.sol' not found. Abort!";
+    
+    ### Check if subclass and requested function exists before initialize node and execute it.
+    my ($reqPackage,$reqSubclass,$reqFunc) = ( $json->{meta}{postdata}{method} =~ /(\w+)\.(\w+)\.(\w+)/ );
+    my ($subclass) = grep { $json->{meta}{postdata}{method} =~ /^\w+\.($_)(\.\w+)?$/ }  map /methods\/eth\/(\w+)\.pm/, keys %INC;
+    if( defined $subclass ) {
+        my $subclass_func = $1 if( $json->{meta}{postdata}{method} =~ /^eth\.contract\.(\w+)/ );
+        my @subs;
+        {
+            no strict 'refs';
+            my $class = 'API::methods::'.$reqPackage.'::'.$subclass.'::';
+            @subs = keys %$class;
+        }
+        if( grep { $_ eq $subclass_func } @subs ) {
+            $json->{meta}{method} = $json->{meta}{postdata}{method};
+            
+            ### Initialize Ethereum Node
+            my $node = API::modules::Ethereum->new('http://127.0.0.1:854'.($API::dev?6:5).'/');
+            $node->set_debug_mode(1);
+            $node->set_show_progress(1);
+            
+            {
+                no strict 'refs';
+                my $method_run_ref = \&{"API::methods::eth::${subclass}::${subclass_func}"};
+                my $method_run_result = $method_run_ref->($cgi,$json,$eth,$node);
+                if( ref($method_run_result) eq 'HASH' ) {
+                    $json->{meta}{rc}  = $method_run_result->{rc};
+                    $json->{meta}{msg} = $method_run_result->{msg};
                 }
             }
+            
         } else {
             $json->{meta}{rc}  = 400;
-            $json->{meta}{msg} = "No 'params' object{} for method-parameter submitted. Abort!";
+            $json->{meta}{msg} = "Requested function '".($reqFunc || '')."' does not exist in package '$reqPackage.$subclass'. Abort!";
         }
+    } else {
+            $json->{meta}{rc}  = 400;
+            $json->{meta}{msg} = "Requested class '".($reqSubclass || '')."' does not exist in package '".($reqPackage || '')."'. Abort!";
     }
     
-    #########################################################################
-    
+    ###
     return {
        data => $eth,
     };
---
+++ methods/eth/contract.pm
@@ -0,0 +1,60 @@
+package API::methods::eth::contract;
+
+use strict; use warnings; use utf8; use feature ':5.10';
+
+
+sub deploy {
+    my $cgi=shift; my $json=shift; my $eth=shift; my $node=shift;
+    
+    my $params = $json->{meta}{postdata}{params} || undef;
+    unless( ref($params) eq 'HASH' ) {
+        return { 'rc' => 400, 'msg' => "No 'params' object{} for method-parameter submitted. Abort!" };
+    } else {
+        unless( $params->{name} ) {
+            return { 'rc' => 400, 'msg' => "Insufficient arguments submitted: 'name' of contract to deploy is needed!" };
+        }
+    }
+
+    unless( -e 'contracts/'.$params->{name}.'.sol' ) {
+        return { 'rc' => 400, 'msg' => "Contract 'contracts/$params->{name}.sol' not found. Abort!" };
+    }
+
+    my $contract_status;
+    eval {
+        $contract_status = $node->compile_and_deploy_contract(
+            $params->{name},
+            {   # Constructor Params
+                initString => '+ IceMine.io - The One And Only +',
+                initValue  => 102,
+            },
+            API::methods::eth::personal::account::address,
+            API::methods::eth::personal::account::password
+        ); 1; 
+    } or do {
+        return { 'rc' => 500, 'msg' => "error.eth.contract.deploy: ".$@ };
+    };
+    my $address     = $contract_status->{contractAddress};
+    my $txhash      = $contract_status->{transactionHash};
+    my $gas_used    = hex($contract_status->{gasUsed});
+    my $gas_price   = $node->eth_gasPrice();
+    my $tx_cost_wei = $gas_used * $gas_price;
+    my $tx_cost_eth = $node->wei2ether($tx_cost_wei);
+    $eth->{contract}{deploy}{address}       = $address;
+    $eth->{contract}{deploy}{tx}            = $txhash;
+    $eth->{contract}{deploy}{tx_cost_wei}   = $tx_cost_wei->numify();
+    $eth->{contract}{deploy}{tx_cost_eth}   = $tx_cost_eth->numify();
+    $eth->{contract}{deploy}{gas_price_wei} = $gas_price->numify();
+    $eth->{contract}{deploy}{gas_used}      = $gas_used;
+}
+
+sub test {
+    my $cgi=shift; my $json=shift; my $eth=shift; my $node=shift;
+    my $mathBigIntOBject = $node->eth_getBalance( API::methods::eth::personal::account::address, 'latest' );
+    # $json->{meta}{msg} = Dumper(  );
+    # $json->{meta}{msg} = $node->wei2ether( $mathBigIntOBject )->bstr(); # ->numify()
+    $json->{meta}{msg} = $mathBigIntOBject->bstr();
+    # $json->{meta}{msg} = $node->web3_clientVersion();
+}
+
+
+1;
