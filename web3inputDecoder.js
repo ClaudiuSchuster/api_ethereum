51e7f59291db810016b09dacd0f8b60cad6eff00
ClaudiuSchuster <info@x0e.de>
2018-07-22 01:27:47 +0000

    cleanup

--- web3inputDecoder.js
+++ web3inputDecoder.js
@@ -34,3 +34,20 @@ port = args[0];
 host = '0.0.0.0';
 eventDecoder.listen(port, host);
 console.log('eventDecoder listening at http://' + host + ':' + port);
+
+
+
+
+
+
+
+// #!/usr/bin/nodejs
+
+// var args = process.argv.slice(2);
+
+// const InputDataDecoder = require('ethereum-input-data-decoder');
+// const decoder = new InputDataDecoder('./contracts/' + args[0] + '.abi');
+// const result = decoder.decodeData(args[1]);
+
+
+// process.stdout.write( JSON.stringify(result, null, 4) );
